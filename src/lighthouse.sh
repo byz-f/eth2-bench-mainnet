a8c371b55dc57583bcb5e70928e51900016dd885
q9f <58883403+q9f@users.noreply.github.com>
2021-01-29 11:41:03 +0100

    update scripts

--- src/lighthouse.sh
+++ src/lighthouse.sh
@@ -4,7 +4,7 @@ while true
 do
   lighthouse bn \
   --datadir /srv/chain/lighthouse \
-  --eth1-endpoints https://mainnet.infura.io/v3/9352a830436b4171b3bcd96b2f418790 \
+  --eth1-endpoints https://mainnet.infura.io/v3/819412e57b5943a6a12c0a335fe5f87b \
   --http --eth1 --metrics 2>&1 | tee ../log/$(date +%y%m%d%H%M%S)-lighthouse.log
   sleep 10
 done
--- src/prysm.sh
+++ src/prysm.sh
@@ -3,7 +3,8 @@
 while true
 do
   beacon-chain \
+  --accept-terms-of-use \
   --datadir /srv/chain/prysm \
-  --http-web3provider https://mainnet.infura.io/v3/9352a830436b4171b3bcd96b2f418790 2>&1 | tee ../log/$(date +%y%m%d%H%M%S)-prysm.log
+  --http-web3provider https://mainnet.infura.io/v3/492f687207ad4d9eb6a9f27ebeb73e5a 2>&1 | tee ../log/$(date +%y%m%d%H%M%S)-prysm.log
   sleep 10
 done
